644be2f98b61d5daf49410ea0672f1ff59cd2fa7
Abdul Rehman <119300719+sheikhere@users.noreply.github.com>
2023-10-26 12:22:36 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import { currencies, amount } from "./input.js";
+import { currency, greeting } from "./function.js";
+
+
+//--------------------------------------------
+
+
+
+greeting();
+
+currency(currencies.currency,amount.money);
+
+
